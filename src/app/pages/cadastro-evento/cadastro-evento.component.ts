76709119d19ea76464af87bb59a5805f1822eb5d
kelly <kellycfs@gmail.com>
2018-06-24 16:14:58 -0300

    ei

--- src/app/pages/cadastro-evento/cadastro-evento.component.ts
+++ src/app/pages/cadastro-evento/cadastro-evento.component.ts
@@ -1,7 +1,7 @@
 import { EventoService } from './../../services/evento.service';
 import { Evento } from './../../model/evento';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-evento',
@@ -10,12 +10,13 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
 })
 export class CadastroEventoComponent implements OnInit {
 
+  submitted = false;
 
   formEvento = new FormGroup({
-    nome: new FormControl(),
-    descricao: new FormControl(),
+    nome: new FormControl('', Validators.required),
+    descricao: new FormControl('', Validators.required),
     local: new FormGroup({
-      nome: new FormControl(),
+      nome: new FormControl('', Validators.required),
       endereco: new FormGroup({
         logradouro: new FormControl(),
         complemento: new FormControl(),
@@ -25,17 +26,20 @@ export class CadastroEventoComponent implements OnInit {
         cep: new FormControl(),
       })
     }),
-    dataInicio: new FormControl(),
-    horaInicio: new FormControl(),
+    dataInicio: new FormControl('', Validators.required),
+    horaInicio: new FormControl('', Validators.required),
     link: new FormControl(),
     banner: new FormControl(),
     ritmos: new FormArray([
-      this.initRitmo()
+      // this.initRitmo()
     ]),
     professores: new FormArray([
-      this.initProfessor()
+      // this.initProfessor()
     ])
   });
+
+  // convenience getter for easy access to form fields
+  get f() { return this.formEvento.controls; }
 
   initRitmo() {
 
@@ -77,31 +81,39 @@ export class CadastroEventoComponent implements OnInit {
 
   save() {
     const evento = this.prepareSaveEvento();
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.formEvento.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)');
     // call API to save
     this.eventoService.save(evento);
     console.log(evento);
-}
+  }
 
 
-prepareSaveEvento(): Evento {
-  const evento = this.formEvento.value;
+  prepareSaveEvento(): Evento {
+    const evento = this.formEvento.value;
 
-  /*// deep copy of form model lairs
-  const secretLairsDeepCopy: Address[] = formModel.secretLairs.map(
-    (address: Address) => Object.assign({}, address)
-  );
-
-  // return new `Hero` object containing a combination of original hero value(s)
-  // and deep copies of changed form model values
-  const saveHero: Hero = {
-    id: this.hero.id,
-    name: formModel.name as string,
-    // addresses: formModel.secretLairs // <-- bad!
-    addresses: secretLairsDeepCopy
-  };
-  return saveHero;
-  */
- return evento;
-}
+    /*// deep copy of form model lairs
+    const secretLairsDeepCopy: Address[] = formModel.secretLairs.map(
+      (address: Address) => Object.assign({}, address)
+    );
+  
+    // return new `Hero` object containing a combination of original hero value(s)
+    // and deep copies of changed form model values
+    const saveHero: Hero = {
+      id: this.hero.id,
+      name: formModel.name as string,
+      // addresses: formModel.secretLairs // <-- bad!
+      addresses: secretLairsDeepCopy
+    };
+    return saveHero;
+    */
+    return evento;
+  }
 
 }
